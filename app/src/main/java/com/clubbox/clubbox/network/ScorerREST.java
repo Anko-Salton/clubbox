c9ed6ead7408d55dfc1da9426d910cc8cab264b3
Anko-Salton <timweber@live.fr>
2016-10-05 11:00:05 +0200

    Scrorer REST added v2

--- app/src/main/java/com/clubbox/clubbox/network/ScorerREST.java
+++ app/src/main/java/com/clubbox/clubbox/network/ScorerREST.java
@@ -1,7 +1,9 @@
 package com.clubbox.clubbox.network;
 
+import com.clubbox.clubbox.model.Scorer;
 import com.clubbox.clubbox.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,7 +11,7 @@ import retrofit.Call;
 import retrofit.http.GET;
 import retrofit.http.Path;
 
-/**
+/*
  * Created by cdsm16 on 05/10/2016.
  */
 
@@ -17,7 +19,7 @@ public interface ScorerREST {
     public static final String ENDPOINT = "http://10.0.2.2:8180/webservice/";
 
     @GET("/club/{idClub}/scorers")
-    Call<Map<Integer,User>> listScorerByClub(@Path("idClub") Integer idClub);
+    Call<ArrayList<Map<Integer,User>>> listScorerByClub(@Path("idClub") Integer idClub);
     @GET("/match/{idMatch}/scorers")
-    Call<List<User>> listScorerByMatch(@Path("idMatch") Integer idMatch);
+    Call<List<Scorer>> listScorerByMatch(@Path("idMatch") Integer idMatch);
 }
